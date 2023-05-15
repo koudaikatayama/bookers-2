ad4ec7922e448017669b1bb9ba414a73cd86b09f
KoudaiKatayama <132341723+koudaikatayama@users.noreply.github.com>
2023-05-15 07:11:59 +0000

    [stop2]bookers

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,9 +4,13 @@ class BooksController < ApplicationController
   end
   
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to '/'
+    @book = Book.new(book_params)
+    if @book.save
+     flash[:notice]= "Book was successfully created."
+     redirect_to book_path(@book.id)
+    else
+     render :new
+    end
   end
 
   def index
@@ -24,8 +28,10 @@ class BooksController < ApplicationController
   
   def update
     book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    if book.update(book_params)
+      flash[:notice]= "Book was successfully update."
+      redirect_to book_path(book.id)
+    end
   end
   
   def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
   resources :books
   
   get 'books/new'
+  get '/top'=>'homes#top'
   post 'books' => 'books#create'
   get 'books'=>'books#index'
-  get '/top'=>'homes#top'
   get 'books/:id'=>'books#show', as:'list'
   get 'books/:id/edit'=> 'books#edit', as:'edit_list'
   patch 'books/:id'=>'book#update', as:'update_list'
